195d3a69bad6539b3a962492b0e0a41cb6298e81
Michelle Setúbal <missetubal@outlook.com>
2022-04-23 12:31:20 -0300

    chore: add button view

--- src/pages/Home/home.screen.tsx
+++ src/pages/Home/home.screen.tsx
@@ -1,7 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 // import {Discord} from '../../assets/discord';
-import {Gradiente, Images, LoginImage} from './home.style';
+import {ButtonsView, Gradiente, Images, LoginImage} from './home.style';
 import {Text, Button, Title, View} from '../../style/general.style';
 import colors from '../../style/colors';
 
@@ -17,20 +17,22 @@ const Home: React.FC = () => {
       <Text marginLeft={64} marginRight={64} marginTop={16} marginBottom={48}>
         Crie grupos para jogar seus games favoritos com os seus amigos
       </Text>
-      <Button
-        backgroundColor={colors.redish}
-        onPress={() => navigation.navigate('Login')}>
-        <Text fontWeight={600} fontSize={18}>
-          Entrar
-        </Text>
-      </Button>
-      <Button
-        onPress={() => navigation.navigate('Register')}
-        backgroundColor={'transparent'}
-        justifyContent={'flex-start'}
-        marginTop={5}>
-        <Text fontWeight={600}>Criar conta</Text>
-      </Button>
+      <ButtonsView>
+        <Button
+          backgroundColor={colors.redish}
+          onPress={() => navigation.navigate('Login')}>
+          <Text fontWeight={600} fontSize={18}>
+            Entrar
+          </Text>
+        </Button>
+        <Button
+          onPress={() => navigation.navigate('Register')}
+          backgroundColor={'transparent'}
+          justifyContent={'flex-start'}
+          marginTop={5}>
+          <Text fontWeight={600}>Criar conta</Text>
+        </Button>
+      </ButtonsView>
     </View>
   );
 };
